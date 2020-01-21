cd9358557a1ba1978e782f02992979701234a8fb
Mike15234 <miguelantonio1852@gmail.com>
2020-01-21 18:33:01 -0500

    Clase peliculas e instancia

---
+++ Lab0MiguelRecinos/Peliculas/Peliculas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab0MiguelRecinos.Peliculas
+{
+    public class Peliculas
+    {
+        private static Peliculas _instancia;
+        public static Peliculas Instancia { get { if (_instancia == null) _instancia = new Peliculas();
+                return _instancia;
+            } }
+        public string Nombre;
+        public string Estreno;
+        public string Director;
+    }
+}
